73ccd6b1367d4d36b167904ef324d64a3343cff5
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-07-25 11:17:51 -0400

    Update scripts to handle morpho

--- bin/mb_register.sh
+++ bin/mb_register.sh
@@ -6,13 +6,33 @@ movingfile=$1
 fixedfile=$2
 outputdir=$3
 
+#Define location of morpho transforms
+initalmovingxfm=$(dirname $(dirname $outputdir))/modelspace/$(basename $movingfile)0_GenericAffine.xfm
+initalfixedxfm=$(dirname $(dirname $outputdir))/modelspace/$(basename $fixedfile)0_GenericAffine.xfm
+
+#If morpho transforms exist, use those to bootstrap registration
+if [[ -s $initalmovingxfm && -s $initalfixedxfm ]]
+then
+stages=$(cat << EOF
+--initial-moving-transform $initalmovingxfm --initial-fixed-transform $initalfixedxfm
+--transform SyN[0.5,3,0] --metric CC[$fixedfile,$movingfile,1,4] --convergence [100x100x100x20,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 4x2x1x0
+EOF
+)
+else
+stages=$(cat << EOF
+--initial-moving-transform [$fixedfile,$movingfile,1] \
+--transform Rigid[0.1] --metric Mattes[$fixedfile,$movingfile,1] --convergence [2000x2000x2000x2000,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 8x4x2x1 \
+--transform Similarity[0.1] --metric Mattes[$fixedfile,$movingfile,1] --convergence [2000x2000x2000x2000,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 8x4x2x1 \
+--transform Affine[0.1] --metric Mattes[$fixedfile,$movingfile,1] --convergence [2000x2000x2000x2000x2000,1e-6,10] --shrink-factors 8x4x2x1x1 --smoothing-sigmas 8x4x2x1x0 \
+--transform SyN[0.5,3,0] --metric CC[$fixedfile,$movingfile,1,4] --convergence [100x100x100x20,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 4x2x1x0
+EOF
+)
+fi
+
 antsRegistration --dimensionality 3 --float 0 --collapse-output-transforms 1 ${MB_VERBOSE:-} --minc \
   --output [$outputdir/$(basename $movingfile)-$(basename $fixedfile)] \
   --winsorize-image-intensities [0.01,0.99] --use-histogram-matching 1 \
-  --initial-moving-transform [$fixedfile,$movingfile,1] \
-  --transform Rigid[0.1] --metric Mattes[$fixedfile,$movingfile,1] --convergence [2000x2000x2000x2000,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 8x4x2x1 \
-  --transform Similarity[0.1] --metric Mattes[$fixedfile,$movingfile,1] --convergence [2000x2000x2000x2000,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 8x4x2x1 \
-  --transform Affine[0.1] --metric Mattes[$fixedfile,$movingfile,1] --convergence [2000x2000x2000x2000x2000,1e-6,10] --shrink-factors 8x4x2x1x1 --smoothing-sigmas 8x4x2x1x0 \
-  --transform SyN[0.5,3,0] --metric CC[$fixedfile,$movingfile,1,4] --convergence [100x100x100x20,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 4x2x1x0 && \
-  rm $outputdir/$(basename $movingfile)-$(basename $fixedfile)*inverse*
-  #Inverses are never used, remove them right after creation (if only I could disable creation...)
+  ${stages} && \
+ImageMath 3 $outputdir/$(basename $movingfile)-$(basename $fixedfile)0_GenericAffine.xfm MakeAffineTransform 1 && \
+rm $outputdir/$(basename $movingfile)-$(basename $fixedfile)*inverse*
+# Inverses are never used, remove them right after creation (if only I could disable creation...)
--- bin/mb_resample.sh
+++ bin/mb_resample.sh
@@ -11,36 +11,62 @@ subjectname=$(basename $subject)
 atlasname=$(basename $atlas)
 templatename=$(basename $template)
 
-#Here, we do a check for the presence of morpho transforms, if they exist
-#If they do, push a transform to the end of the transform stack
+# Here, we do a check for the presence of morpho transforms, if they exist we
+# will only have nonlinear transforms in model space
 if [[ -s output/transforms/modelspace/${subjectname}0_GenericAffine.xfm ]]
 then
-    morphosubjecttransform=" -t [output/transforms/modelspace/${subjectname}0_GenericAffine.xfm,1]"
-else
-    morphosubjecttransform=""
-fi
-
-
-#Check for subjectname == $templatename, if so, we skipped that registration, so don't apply those transforms
-if [[ ${subjectname} = ${templatename} ]]
-then
-antsApplyTransforms -d 3  ${MB_VERBOSE:-} --interpolation GenericLabel -r ${subject} \
+  if [[ ${subjectname} = ${templatename} ]]
+  then
+    # Apply transforms to label
+    # atlas to model (affine)
+    # atlas to template/subject (nonlinear)
+    # model to subject (affine)
+    antsApplyTransforms -d 3  ${MB_VERBOSE:-} --interpolation GenericLabel -r ${subject} \
     -i $(echo $atlas | sed -E "s/t1\.(nii|nii\.gz|mnc)/${labelname}/g") \
-    -o /tmp/${atlasname}-${templatename}-${subjectname}-$labelname \
-    ${morphosubjecttransform} \
+    -o /tmp/${atlasname}-${templatename}-${subjectname}-${labelname} \
+    -t [output/transforms/modelspace/${subjectname}0_GenericAffine.xfm,1] \
+    -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}0_NL.xfm \
+    -t output/transforms/modelspace/${atlasname}0_GenericAffine.xfm
+  else
+    # Apply transforms to label
+    # atlas to model (affine)
+    # atlas to template (nonlinear)
+    # template to subject (nonlinear)
+    # model to subject (affine)
+    antsApplyTransforms -d 3  ${MB_VERBOSE:-} --interpolation GenericLabel -r ${subject} \
+    -i $(echo $atlas | sed -E "s/t1\.(nii|nii\.gz|mnc)/${labelname}/g") \
+    -o /tmp/${atlasname}-${templatename}-${subjectname}-${labelname} \
+    -t [output/transforms/modelspace/${subjectname}0_GenericAffine.xfm,1] \
+    -t output/transforms/template-subject/${subjectname}/${templatename}-${subjectname}0_NL.xfm \
+    -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}0_NL.xfm \
+    -t output/transforms/modelspace/${atlasname}0_GenericAffine.xfm
+  fi
+else
+  if [[ ${subjectname} = ${templatename} ]]
+  then
+    # Apply transforms to label
+    # atlas to template/subject (affine)
+    # atlas to template/subject (nonlinear)
+    antsApplyTransforms -d 3  ${MB_VERBOSE:-} --interpolation GenericLabel -r ${subject} \
+    -i $(echo $atlas | sed -E "s/t1\.(nii|nii\.gz|mnc)/${labelname}/g") \
+    -o /tmp/${atlasname}-${templatename}-${subjectname}-${labelname} \
     -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}1_NL.xfm \
     -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}0_GenericAffine.xfm
-else
-#Transforms are applied like matrix algebra, last transform on the command line is applied first
-antsApplyTransforms -d 3  ${MB_VERBOSE:-} --interpolation GenericLabel -r ${subject} \
+  else
+    # Apply transforms to label
+    # atlas to template (affine)
+    # atlas to template (nonlinear)
+    # template to subject (affine)
+    # template to subject (nonlinear)
+    antsApplyTransforms -d 3  ${MB_VERBOSE:-} --interpolation GenericLabel -r ${subject} \
     -i $(echo $atlas | sed -E "s/t1\.(nii|nii\.gz|mnc)/${labelname}/g") \
-    -o /tmp/${atlasname}-${templatename}-${subjectname}-$labelname \
-    ${morphosubjecttransform} \
+    -o /tmp/${atlasname}-${templatename}-${subjectname}-${labelname} \
     -t output/transforms/template-subject/${subjectname}/${templatename}-${subjectname}1_NL.xfm \
     -t output/transforms/template-subject/${subjectname}/${templatename}-${subjectname}0_GenericAffine.xfm \
     -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}1_NL.xfm \
     -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}0_GenericAffine.xfm
+  fi
 fi
 
-ConvertImage 3 /tmp/${atlasname}-${templatename}-${subjectname}-$labelname output/labels/candidates/${subjectname}/${atlasname}-${templatename}-${subjectname}-$labelname 1
-rm /tmp/${atlasname}-${templatename}-${subjectname}-$labelname
+ConvertImage 3 /tmp/${atlasname}-${templatename}-${subjectname}-${labelname} output/labels/candidates/${subjectname}/${atlasname}-${templatename}-${subjectname}-${labelname} 1
+rm /tmp/${atlasname}-${templatename}-${subjectname}-${labelname}
